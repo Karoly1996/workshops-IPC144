51bf4496a1f357121cd8151772f2c2ff6f439e5b
Karoly <120995235+Karoly1996@users.noreply.github.com>
2023-06-05 01:16:11 -0400

    Add files via upload

--- w5p1.c
+++ w5p1.c
@@ -1,9 +1,9 @@
 /*/////////////////////////////////////////////////////////////////////////
                           Workshop - #5 (P1)
-Full Name  : 
-Student ID#: 
-Email      : 
-Section    : 
+Full Name  : Karoly Nemeth
+Student ID#: 021949144
+Email      : knemeth@myseneca.ca
+Section    : NDD
 
 Authenticity Declaration:
 I declare this submission is the result of my own work and has not been
@@ -19,6 +19,7 @@ piece of work is entirely of my own creation.
 
 int main(void)
 {
+    //Assigning my variables
     int const JAN = 1, DEC = 12;
     int continueLoop = 1;
     int year, month;
@@ -27,31 +28,70 @@ int main(void)
     printf("General Well-being Log\n");
     printf("======================\n");
 
-    //Loop begins
+    //Start the loop
     while (continueLoop == 1) {
+        //Ask for user input for year and month
         printf("Set the year and month for the well-being log (YYYY MM): ");
         scanf("%d %d", &year, &month);
 
+        //Checking to make sure year is equal to or between 2012 and 2022
         if (year < MIN_YEAR || year > MAX_YEAR) {
+            //Print error if user inputs under 2012 or over 2022
             printf("ERROR: The year must be between 2012 and 2022 inclusive\n");
+            //Checks to see if user inputs a month between 1 and 12
         } if (month < JAN || month > DEC) {
+            //Print error message if its not between 1 and 12
             printf("ERROR: Jan.(1) - Dec.(12)\n");
+            //Checks to see if both year and months are valid inputs
+        } else if ((year >= MIN_YEAR && year <= MAX_YEAR) && (month >= JAN && month <= DEC)) { 
             printf("\n");
-        } else {
             printf(">*** Log date set! ***\n\n");
-            printf("Log starting date: %d-%d-01\n", year, month);
             
+            
+            //Using switch statements to check each case individually
             switch (month) {
+                //Display the 3 letter month depending which month user input
             case 1:
-                printf("JAN");
+                printf("Log starting date: %d-JAN-01\n", year);
                 break;
-            
-            default:
+            case 2:
+                printf("Log starting date: %d-FEB-01\n", year);
+                break;
+            case 3:
+                printf("Log starting date: %d-MAR-01\n", year);
+                break;
+            case 4:
+                printf("Log starting date: %d-APR-01\n", year);
+                break;
+            case 5:
+                printf("Log starting date: %d-MAY-01\n", year);
+                break;
+            case 6:
+                printf("Log starting date: %d-JUN-01\n", year);
+                break;
+            case 7:
+                printf("Log starting date: %d-JUL-01\n", year);
+                break;
+            case 8:
+                printf("Log starting date: %d-AUG-01\n", year);
+                break;
+            case 9:
+                printf("Log starting date: %d-SEP-01\n", year);
+                break;
+            case 10:
+                printf("Log starting date: %d-OCT-01\n", year);
+                break;
+            case 11:
+                printf("Log starting date: %d-NOV-01\n", year);
+                break;
+            case 12:
+                printf("Log starting date: %d-DEC-01\n", year);
                 break;
             }
-            break;
+            //End loop
+            continueLoop = 0;
         }
-    } 
+    }
 
 
     
